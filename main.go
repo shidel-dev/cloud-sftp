ad65120b8d8e5126ccea4c47fcb6ccdb7c3218e2
shidel-dev <joeshidel@gmail.com>
2020-01-20 10:00:07 -0800

    add imports for azureblob, and gcsblob

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/shidel-dev/cloud-sftp/cmd"
+	_ "gocloud.dev/blob/azureblob"
 	_ "gocloud.dev/blob/fileblob"
+	_ "gocloud.dev/blob/gcsblob"
 	_ "gocloud.dev/blob/memblob"
 	_ "gocloud.dev/blob/s3blob"
 )
